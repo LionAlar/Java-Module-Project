21061775a51ea8676a63dc59c5e060137a3800c7
LionAlar <dark-space2@yandex.ru>
2024-07-01 06:22:02 +0300

    Созданы 3 класса: Car - автомобиль.   * Поля - название и скорость   * Методы - геттеры и сеттеры полей, проверка скорости на корректность и расчет пройденной дистанции за 24 часа Race - гонка.   *Поля - ArrayList с машинами.   * Методы - добавление нового автомобиля в список. возврат победителя гонки Main - Главный класс проекта.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,54 @@
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+    public static void main(String[] args) throws UnsupportedEncodingException {
+
+        // Тщетные попытки утсановить кириллицу в консоле :)
+        /*
+        String ru = "Русский язык";
+        PrintStream ps = new PrintStream(System.out, true, "UTF-8");
+        System.out.println(ru.length());
+        System.out.println(ru);
+        ps.println(ru);
+
+        // Да, тут дальше идет немецкий и потом вообще корейский,
+        // но это должны были быть рабочие примеры с формумов, но увы и даже они не пашут :(
+
+        String inputString = "Entwickeln Sie mit Vergnügen";
+        byte[] bytes = inputString.getBytes(StandardCharsets.UTF_8);
+
+        String utf8EncodedString = new String(bytes, StandardCharsets.UTF_8);
+        System.out.println("German string is :: " + inputString);
+        System.out.println("utf8 Encoded String is ::" + utf8EncodedString);
+        System.out.println("Both String Equals:: " + inputString.equals(utf8EncodedString));
+
+        String str = "hey\u6366";
+        byte[] charset = str.getBytes("UTF-8");
+        String result = new String(charset, "UTF-8");
+        System.out.println(result);
+
+         */
+
+
+        Scanner scanner = new Scanner(System.in);
+        Race race = new Race();
+        String name;
+        double speed;
+        Car car;
+        for(int i = 0;i < 3;i++) {
+            System.out.println("Name of car number " + (i+1) + ":");
+            name = scanner.next();
+            do {
+                System.out.println("Speed of car number " + (i+1) + ":");
+                speed = scanner.nextDouble();
+                car = new Car(name, speed);
+                if(!car.isCorrectSpeed()) System.out.println("Incorrect speed!");
+            } while (!car.isCorrectSpeed());
+            race.addCar(car);
+        }
+        Car winner = race.getWinner();
+        System.out.println("The faster car -> " + winner.getName());
     }
 }
---
+++ src/main/java/Race.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Race {
+    private List<Car> cars = new ArrayList<>();
+
+    public Race() {
+    }
+
+    public void addCar(Car car){
+        cars.add(car);
+    }
+
+    public Car getWinner(){
+        if (cars.size() == 0) return null;
+        Car winner = cars.get(0);
+        for (Car car: cars) {
+            if (car.getDistance() > winner.getDistance())
+                winner = car;
+        }
+        return winner;
+    }
+}

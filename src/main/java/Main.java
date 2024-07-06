3bb55c758ce8b4da4e4622b88892a90014e8a9a9
LionAlar <dark-space2@yandex.ru>
2024-07-06 13:42:31 +0300

    Созданы 3 класса: Car - автомобиль.   * Поля - название и скорость   * Методы - геттеры и сеттеры полей, проверка скорости на корректность и расчет пройденной дистанции за 24 часа Race - гонка.   *Поля - ArrayList с машинами.   * Методы - добавление нового автомобиля в список. возврат победителя гонки Main - Главный класс проекта.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,34 +4,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws UnsupportedEncodingException {
-
-        // Тщетные попытки утсановить кириллицу в консоле :)
-        /*
-        String ru = "Русский язык";
-        PrintStream ps = new PrintStream(System.out, true, "UTF-8");
-        System.out.println(ru.length());
-        System.out.println(ru);
-        ps.println(ru);
-
-        // Да, тут дальше идет немецкий и потом вообще корейский,
-        // но это должны были быть рабочие примеры с формумов, но увы и даже они не пашут :(
-
-        String inputString = "Entwickeln Sie mit Vergnügen";
-        byte[] bytes = inputString.getBytes(StandardCharsets.UTF_8);
-
-        String utf8EncodedString = new String(bytes, StandardCharsets.UTF_8);
-        System.out.println("German string is :: " + inputString);
-        System.out.println("utf8 Encoded String is ::" + utf8EncodedString);
-        System.out.println("Both String Equals:: " + inputString.equals(utf8EncodedString));
-
-        String str = "hey\u6366";
-        byte[] charset = str.getBytes("UTF-8");
-        String result = new String(charset, "UTF-8");
-        System.out.println(result);
-
-         */
-
-
         Scanner scanner = new Scanner(System.in);
         Race race = new Race();
         String name = "default";
